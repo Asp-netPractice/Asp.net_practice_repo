5e60b7f2012ca098e73310e33a0202949123462f
Nayan <nayanthakre379@gmail.com>
2025-01-03 23:05:23 +0530

    Merge branch 'main' of https://github.com/Asp-netPractice/Asp.net_practice_repo

--- WebApplication4/Loginpg.aspx.cs
+++ WebApplication4/Loginpg.aspx.cs
@@ -6,19 +6,17 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace WebApplication4
-
 {
-    public partial class Login :System.Web.UI.Page
+    public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
         }
 
-        protected void Button2_Click(object sender, EventArgs e)
+        protected void Unnamed_Click(object sender, EventArgs e)
         {
-            
-        }
 
-       
+        }
     }
 }
--- WebApplication4/Loginpg.aspx.designer.cs
+++ WebApplication4/Loginpg.aspx.designer.cs
@@ -11,34 +11,34 @@ namespace WebApplication4
 {
 
 
-    public partial class _Default
+    public partial class WebForm1
     {
 
         /// <summary>
-        /// formGroupExampleInput control.
+        /// email_id control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText formGroupExampleInput;
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl email_id;
 
         /// <summary>
-        /// password control.
+        /// pwd control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputPassword password;
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword pwd;
 
         /// <summary>
-        /// Button2 control.
+        /// exampleCheck1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.HtmlControls.HtmlInputCheckBox exampleCheck1;
     }
 }
