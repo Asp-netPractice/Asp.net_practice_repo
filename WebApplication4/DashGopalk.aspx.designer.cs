55086de2516ef0a2066d254f49f3c69ae5f7db6f
Gkharwade <gkharwade18@gmail.com>
2025-01-06 15:59:22 +0530

    INSERT OPERATION ADDED

--- WebApplication4/DashGopalk.aspx.designer.cs
+++ WebApplication4/DashGopalk.aspx.designer.cs
@@ -22,5 +22,41 @@ namespace WebApplication4
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Literal litUsername;
+
+        /// <summary>
+        /// stdname control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText stdname;
+
+        /// <summary>
+        /// rollno control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText rollno;
+
+        /// <summary>
+        /// marks control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText marks;
+
+        /// <summary>
+        /// subject control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText subject;
     }
 }
